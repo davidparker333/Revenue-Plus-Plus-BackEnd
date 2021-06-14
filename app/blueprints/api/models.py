6ec7a174d181a8699a2b344acfe4d644fe405391
davidparker333 <davidparker333@gmail.com>
2021-06-14 14:48:43 -0400

    opportunity, event, activity models

--- app/blueprints/api/models.py
+++ app/blueprints/api/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy.sql.operators import op
 from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash
@@ -9,6 +10,11 @@ import base64
 @login.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
+
+
+# #############################################
+#                  USER MODEL 
+# #############################################
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -63,6 +69,9 @@ class User(db.Model, UserMixin):
                 setattr(self, field, data[field])
 
 
+# #############################################
+#                  LEAD MODEL 
+# #############################################
 
 class Lead(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -73,10 +82,12 @@ class Lead(db.Model):
     business_name = db.Column(db.String(200), nullable=False)
     address = db.Column(db.String(255), nullable=False)
     status = db.Column(db.String(100), nullable=False)
-    hot = db.Column(db.Boolean, nullable=False)
+    hot = db.Column(db.Boolean(), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    open = db.Column(db.Boolean, nullable=False)
+    open = db.Column(db.Boolean(), nullable=False)
     date_created = db.Column(db.DateTime(), default=datetime.utcnow())
+    opportunity = db.Relationship('Opportunity', backref='lead', lazy='dynamic')
+    activity = db.Relationship('Activity', backref='lead', lazy='dynamic')
 
     def __init__(self, first_name, last_name, phone_number, business_name, address, status, hot, user_id, open=True, cell_phone_number=None):
        self.first_name = first_name
@@ -123,3 +134,128 @@ class Lead(db.Model):
         db.session.delete(self)
         db.session.commit()
     
+
+# #############################################
+#             OPPORTUNITY MODEL 
+# #############################################
+
+class Opportunity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(150), nullable=False)
+    last_name = db.Column(db.String(150), nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False)
+    cell_phone_number = db.Column(db.String(20))
+    business_name = db.Column(db.String(200), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    status = db.Column(db.String(100), nullable=False)
+    value = db.Column(db.Float(), nullable=False)
+    lead_id = db.Column(db.Integer, db.ForeignKey('lead.id'), nullable=False)
+    date_created = db.Column(db.DateTime(), default=datetime.utcnow())
+    open = db.Column(db.Boolean(), nullable=False)
+    event = db.Relationship('Event', backref='opportunity', lazy='dynamic')
+    activity = db.Relationship('Activity', backref='opportunity', lazy='dynamic')
+
+    def __init__(self, first_name, last_name, phone_number, business_name, address, status, value, lead_id, open=True, cell_phone_number=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.business_name = business_name
+        self.address = address
+        self.status = status
+        self.value = value
+        self.lead_id = lead_id
+        self.open = open
+        self.cell_phone_number = cell_phone_number
+
+    def __repr__(self):
+        return f'<Opportunity Object | {self.business_name}>'
+
+    def __str__(self):
+        return f'Opportunity - {self.id} - {self.business_name}'
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "phone_number": self.phone_number,
+            "cell_phone_number": self.cell_phone_number,
+            "business_name": self.business_name,
+            "address": self.address,
+            "status": self.status,
+            "value": self.value,
+            "date_created": self.date_created
+        }
+
+    def from_dict(self, data):
+        for field in ['first_name', 'last_name', 'phone_number', 'cell_phone_number', 'business_name', 'address', 'status', 'value']:
+            if field in data:
+                setattr(self, field, data[field])
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+# #############################################
+#                  EVENT MODEL 
+# #############################################
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_time = db.Column(db.DateTime(), nullable=False)
+    event_name = db.Column(db.String(150), nullable=False)
+    first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
+    opportunity_id = db.Column(db.Integer, db.ForeignKey('opportunity.id'))
+
+    def __init__(self, date_time, event_name, first_name, last_name, opportunity_id=None):
+        self.date_time = date_time
+        self.event_name = event_name
+        self.first_name = first_name
+        self.last_name = last_name
+        self.opportunity_id = opportunity_id
+
+    def __repr__(self):
+        return f'<Event Object | {self.event_name}>'
+
+    def __str__(self):
+        return f'Event - {self.id} - {self.event_name}'
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "date_time": self.date_time,
+            "first_name": self.first_name,
+            "last_name": self.last_name
+        }
+
+    def from_dict(self, data):
+        for field in ['date_time', 'event_name', 'first_name', 'last_name', 'opportunity_id']:
+            if field in data:
+                setattr(self, field, data[field])
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+# #############################################
+#                ACTIVITY MODEL 
+# #############################################
+
+class Activity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.Date(), nullable=False)
+    notes = db.Column(db.String(2000), nullable=False)
+    lead_id = db.Column(db.Integer, db.ForeignKey('lead.id'))
+    opportunity_id = db.Column(db.Integer, db.ForeignKey('opportunity.id'))
